c148a49a07be6d5cd10e4f59d7bbb8dc21ad9792
Nathan Lounds <loundsna@msu.edu>
2017-06-09 11:22:26 -0400

    Add testing

---
+++ test_webcalc.py
@@ -0,0 +1,19 @@
+"""This module does tests"""
+
+import pytest
+from expecter import expect
+
+from webcalc import app
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+
+def describe_index():
+
+    def it_says_hello(client):
+        response = client.get('/')
+
+        expect(response.data).contains(b"Hello, world!")
