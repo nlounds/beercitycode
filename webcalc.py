f9ededfa61c5a029a2c701aacb12ce593a0fd641
Nathan Lounds <loundsna@msu.edu>
2017-06-09 13:28:29 -0400

    New better test

--- webcalc.py
+++ webcalc.py
@@ -11,6 +11,9 @@ def index():
     """This function says hello"""
     return "Hello, world!"
 
+@app.route('/<int:a>/<op>/<int:b>')
+def calc(a, op, b):
+    return f"Result: {a} {op} {b} = {a + b}"
 
 if __name__ == '__main__':
     app.run(debug=True)
